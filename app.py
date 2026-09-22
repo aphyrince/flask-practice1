b96a86079c662ec576f65ff2290ecf5c55b58307
aphyrince <dkswjdgh01@naver.com>
2026-09-22 09:13:18 +0900

    L4 실습 4 완료~

--- app.py
+++ app.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h1>메인 페이지</h1>"
+
+
+@app.route("/about")
+def about():
+    return "<h1>소개 페이지</h1>"
+
+
+@app.route("/test/<text>")
+def route_sample(text):
+    return text
 
 
 #   이 방법을 쓰려면 flask run 이 아닌, python app.py 방식으로 실행해야함.
